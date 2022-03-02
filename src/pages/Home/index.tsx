afe00f260d61551c5041bc797801dea35da128e7
doctor <wangzhe18@jd.com>
2022-03-02 18:04:26 +0800

    open pages

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import styles from './index.less';
 import Banner from './Banner';
 import Time from './Time';
-// import Roadmap from './Roadmap';
+import Roadmap from './Roadmap';
 import Chart from './Chart';
 import Footer from './Footer';
 export default function IndexPage() {
